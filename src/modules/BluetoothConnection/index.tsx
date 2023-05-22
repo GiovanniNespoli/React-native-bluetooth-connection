4cb0e058f8f485c3d6cf3b21e48b6869f029e7f4
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2023-05-22 15:58:36 -0300

    first commit after expo Prebuild ��

--- src/modules/BluetoothConnection/index.tsx
+++ src/modules/BluetoothConnection/index.tsx
@@ -1,18 +1,25 @@
-import { Button } from '@components/Button';
-import React from 'react';
-import { Container } from './styles';
-import { StackParamList } from '@routes/routesPath';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { Button } from "@components/Button";
+import React, { useCallback, useState } from "react";
+import { Container } from "./styles";
+import { StackParamList } from "@routes/routesPath";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import BleManager from "react-native-ble-manager";
+import { Platform, PermissionsAndroid } from "react-native";
 
 export function BluetoothConnection() {
+  const { navigate, goBack } =
+    useNavigation<StackNavigationProp<StackParamList>>();
 
-    const { navigate, goBack } = useNavigation<StackNavigationProp<StackParamList>>();
-
-    return (
-        <Container>
-            <Button label='Scan here' onPress={() => {  }} />
-            <Button label='Go back' onPress={() => { goBack() }} />
-        </Container>
-    );
-};
+  return (
+    <Container>
+      <Button label="Scan here" onPress={() => {}} />
+      <Button
+        label="Go back"
+        onPress={() => {
+          goBack();
+        }}
+      />
+    </Container>
+  );
+}
--- src/modules/Welcome/index.tsx
+++ src/modules/Welcome/index.tsx
@@ -1,18 +1,22 @@
-import { Button } from '@components/Button';
-import React from 'react';
-import { Container, NavigateText } from './styles';
-import { StackParamList } from '@routes/routesPath';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { Button } from "@components/Button";
+import React from "react";
+import { Container, NavigateText } from "./styles";
+import { StackParamList } from "@routes/routesPath";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
 
 export function Welcome() {
+  const { navigate } = useNavigation<StackNavigationProp<StackParamList>>();
 
-    const { navigate } = useNavigation<StackNavigationProp<StackParamList>>();
-
-    return (
-        <Container>
-            <NavigateText>Want to scan bluetooth devices ?</NavigateText>
-            <Button label='Click here!!' onPress={() => { navigate('BluetoothConnection') }} />
-        </Container>
-    );
-};
+  return (
+    <Container>
+      <NavigateText>Want to scan bluetooth devices ?</NavigateText>
+      <Button
+        label="Click here!!"
+        onPress={() => {
+          navigate("BluetoothConnection");
+        }}
+      />
+    </Container>
+  );
+}
